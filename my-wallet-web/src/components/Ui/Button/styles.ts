24851548daabcb692ab1fdf8de16034ebad404df
christyanms <christyan228@gmail.com>
2025-03-15 21:26:24 -0300

    feat: novos componentes

---
+++ my-wallet-web/src/components/Ui/Button/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+    width: 100%;
+
+    margin: 7px 0;
+    padding: 10px;
+
+    border-radius: 5px;
+
+    font-weight: bold;
+    color: #FFF;
+    background-color: #E44C4E;
+
+    transition: opacity .3s;
+
+    &:hover{
+        opacity: .7;
+    }
+`;
